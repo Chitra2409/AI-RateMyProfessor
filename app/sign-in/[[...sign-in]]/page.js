91950e675f628757ef0d9cac54919c54656aedd4
andleeb4898 <namalandleeb18@gmai.com>
2024-08-24 17:27:06 +0500

    authenticatio added finally

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,26 @@
+//authentication
+
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="signin-container">
+      <div className="header">
+        <h1 className="glowing-text">Welcome to Your Academic AI Partner</h1>
+        <p>Unlock a smarter way to rate and explore professors with AI.</p>
+      </div>
+
+      <div className="signin-content">
+        {/* Left: Background Image or Illustration */}
+        <div className="image-section">
+          <img src="/robot.jpg" alt="Robot" />
+        </div>
+
+        {/* Right: Sign-In Form */}
+        <div className="signin-wrapper">
+          <SignIn />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,26 @@
+//authentication
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="signup-container">
+      <div className="header">
+        <h1 className="glowing-text">Welcome to Your Academic AI Partner</h1>
+        <p>Unlock a smarter way to rate and explore professors with AI.</p>
+      </div>
+
+      <div className="signup-content">
+        {/* Left: Background Image or Illustration */}
+        <div className="image-section">
+          <img src="/robot.jpg" alt="Robot" />
+        </div>
+
+        {/* Right: Sign-Un Form */}
+        <div className="signup-wrapper">
+          <SignUp />
+        </div>
+      </div>
+    </div>
+  );
+}
