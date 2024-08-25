5f184f03dba18087a8870c2bfce400f599e84b15
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2024-08-25 16:43:33 +0200

    Change UI of the form and add fields to the form

--- app/AddProfessor/layout.js
+++ app/AddProfessor/layout.js
@@ -1,12 +1,19 @@
 import Navbar from "../components/Navbar"
-import { Box } from "@mui/material"
+import { Box, Container } from "@mui/material"
 
 // addprofessor layout
 
 export default function AddProfessorLayout({ children }) {
     return (
-        <Box sx={{top: '50vh'}}>
-            {children}
-        </Box>
+        <Container disableGutters sx={{
+            // background: 'linear-gradient(135deg, #6dd5fa, #ffffff)',
+            height: '100vh',
+            width:'100vw',
+        }}>
+
+            <Box sx={{ top: '50vh' }}>
+                {children}
+            </Box>
+        </Container>
     )
 }
--- app/components/ProcessForm.js
+++ app/components/ProcessForm.js
@@ -1,32 +1,32 @@
 'use client'
 import React, { useState } from 'react';
-import { Box, Button, Fade, FormControl, Modal, TextField, Typography } from '@mui/material';
+import { Box, Button, Fade, FormControl, Modal, Stack, TextField, Typography } from '@mui/material';
 
 const ProcessForm = () => {
     const [open, setOpen] = useState(false);
-   
+
     const [record, setRecord] = useState(
-       {
-        name: '',
-        designation: '',
-        profile_image: '',
-        department: '',
-        profile_summary: '',
-        work_experience: '',
-        research_interests: '',
-        teaching_philosophy: '',
-        courses_taught: '',
-        awards_and_grants: '',
-        scholarly_activities: '',
-        contact: ''
-    });
+        {
+            name: '',
+            designation: '',
+            profile_image: '',
+            department: '',
+            profile_summary: '',
+            work_experience: '',
+            research_interests: '',
+            teaching_philosophy: '',
+            courses_taught: '',
+            awards_and_grants: '',
+            scholarly_activities: '',
+            contact: ''
+        });
 
     const handleOpen = () => setOpen(true);
 
     const handleClose = () => setOpen(false);
 
     const handleSubmit = async () => {
-        
+
         fetch('/api/addProfessor', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -55,21 +55,22 @@ const ProcessForm = () => {
                 console.error('Error adding professor:', response.statusText);
             }
         })
-        .catch(error => {
-            // Handle any network or other errors
-            console.error('Network error:', error);
-        });
+            .catch(error => {
+                // Handle any network or other errors
+                console.error('Network error:', error);
+            });
     }
 
     return (
-        <Box display="flex" justifyContent="center" alignItems="center" height="100vh" >
+        <Box display="flex" justifyContent="center" alignItems="center" height="100vh" 
+         >
             <Modal open={open} onClose={handleClose}>
                 <Fade in={open}>
                     <Box
                         sx={{
-                            minWidth: '600px',
-                            height: 500,
-                            p: 2,
+                            minWidth: '60%',
+                            height: screen,
+                             p: 2,
                             display: 'flex',
                             flexDirection: 'column',
                             justifyContent: 'center',
@@ -79,20 +80,35 @@ const ProcessForm = () => {
                             left: '50%',
                             top: '50%',
                             transform: 'translate(-50%, -50%)',
-                            backgroundColor: 'white'
-                            // overflow: 'hidden',
+                            backgroundColor: 'white',
+                            overflow: 'hidden',
 
                         }}
                     >
-                        <Typography variant="h6" sx={{ m: 2, position: 'absolute', top: 2, left: 2, }}>Add a new professor</Typography>
-                        <FormControl position='relative' sx={{ mt: 2 }}>
-                            <TextField label="Name" variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.name} onChange={(e) => setRecord({ ...record, name: e.target.value })} />
-                            <TextField label="Email" variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.contact} onChange={(e) => setRecord({ ...record, contact: e.target.value })} />
-                            <Button type="submit" variant="contained" color="primary"
-                                onClick={handleSubmit}>
-                                Submit
-                            </Button>
-                        </FormControl>
+                        <Typography variant="h5" sx={{position: 'absolute', top: 4, left: 2, fontWeight:600 , background: 'linear-gradient(135deg, #6dd5fa, #ffffff)', width:'100%', padding:2 }}>Add a new professor</Typography>
+                        <Stack position='relative' direction='row' sx={{ mt: 14, display: 'flex', gap: 4, mr: 4 }}>
+                            <Box sx={{ mr: 4, display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'center' }}>
+                                <TextField label="Name" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.name} onChange={(e) => setRecord({ ...record, name: e.target.value })} />
+                                <TextField label="Email" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.contact} onChange={(e) => setRecord({ ...record, contact: e.target.value })} />
+                                <TextField label="Designation" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.designation} onChange={(e) => setRecord({ ...record, designation: e.target.value })} />
+                                <TextField label="Profile image" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.profile_image} onChange={(e) => setRecord({ ...record, profile_image: e.target.value })} />
+                                <TextField label="Department" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.department} onChange={(e) => setRecord({ ...record, department: e.target.value })} />
+                                <TextField label="Profile summary" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.profile_summary} onChange={(e) => setRecord({ ...record, profile_summary: e.target.value })} />
+                            </Box>
+                            <Box sx={{ mr: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                <TextField label="Work Experience" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.work_experience} onChange={(e) => setRecord({ ...record, work_experience: e.target.value })} />
+                                <TextField label="Research Interests" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.research_interests} onChange={(e) => setRecord({ ...record, research_interests: e.target.value })} />
+                                <TextField label="Teaching Philosophy" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.teaching_philosophy} onChange={(e) => setRecord({ ...record, teaching_philosophy: e.target.value })} />
+                                <TextField label="Courses Taught" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.courses_taught} onChange={(e) => setRecord({ ...record, courses_taught: e.target.value })} />
+                                <TextField label="Awards And Grants" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.awards_and_grants} onChange={(e) => setRecord({ ...record, awards_and_grants: e.target.value })} />
+                                <TextField label="Scholarly Activities" multiline rows={3} variant="outlined" sx={{ mb: 2, minWidth: '400px' }} value={record.scholarly_activities} onChange={(e) => setRecord({ ...record, scholarly_activities: e.target.value })} />
+                            </Box>
+                        </Stack>
+                        <Button type="submit" variant="contained" color="primary"
+                        sx={{ width: '30%', height: '100%' , mt:4}}
+                            onClick={handleSubmit}>
+                            Submit
+                        </Button>
                     </Box>
                 </Fade>
             </Modal>
